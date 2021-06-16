9ebdc6c2393a17ec3928ee55a2796778ddfc3d6f
Cesar Torres <71398081+cesartorresr@users.noreply.github.com>
2021-06-16 21:41:51 +0000

    Game

--- main.py
+++ main.py
@@ -18,41 +18,41 @@ print("I'm thinking of a number between 1 and 100. ")
 guess_the_number = random.randint(1,100)
 print(f"believe or not the correct answer is {guess_the_number}")
 
+attempts_hard = 5
+attempts_easy = 10
 
-difficulty = input("Choose the difficulty, type 'easy' or 'hard': ")
-def easy():
-  attempts = 10
-  if difficulty == "easy":
-    for i in range(10):
-      print(f"You have {attempts} remaining to guess the number")
-      guess = int(input("Make a guess: "))
-      attempts -= 1
-      if guess > guess_the_number:
-        print("Too high")
-        print("Guess again")
-      elif guess < guess_the_number:
-        print("Too low")
-        print("Guess again")
-      else:
-        print(f"You got it! The answer was {guess_the_number}")
-        break
-
-def hard():
-  attempts = 5
+def difficulty(difficulty):
   if difficulty == "hard":
-    for i in range(5):
-      print(f"You have {attempts} remaining to guess the number")
+    global attempts_hard
+    while attempts_hard != 0:
+      print(f"You have {attempts_hard} attempts remaining to guess the number.")
       guess = int(input("Make a guess: "))
-      attempts -= 1
-      if guess > guess_the_number:
+      attempts_hard -= 1
+      if guess == guess_the_number:
+        print(f"You got it! The answer was {guess_the_number}")
+      elif attempts_hard == 0:
+        print("You 've run out of guesses. You lose")
+      elif guess > guess_the_number:
         print("Too high")
       elif guess < guess_the_number:
         print("Too low")
-      else:
+  elif difficulty == "easy":
+    global attempts_easy
+    while attempts_easy != 0:
+      print(f"You have {attempts_hard} attempts remaining to guess the number.")
+      guess = int(input("Make a guess: "))
+      attempts_hard -= 1
+      if guess == guess_the_number:
         print(f"You got it! The answer was {guess_the_number}")
-        break
-easy()
-hard()    
+      elif attempts_hard == 0:
+        print("You 've run out of guesses. You lose")
+      elif guess > guess_the_number:
+        print("Too high")
+      elif guess < guess_the_number:
+        print("Too low")
+      
+      
+difficulty(input("Choose a difficulty. Type 'easy' or 'hard': "))
 
 
 
